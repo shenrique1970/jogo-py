cef99e3bc72c20f6fd6902cd89b54aa91619b3e2
Shenrique <79231553+shenrique1970@users.noreply.github.com>
2024-12-13 08:33:19 -0300

    Update jogo.py
    Alteração

--- jogo.py
+++ jogo.py
@@ -12,22 +12,24 @@ r = {1:'Primeira', 2:'Segunda', 3:'Terceira', 4:'Quarta', 5:'Quinta', 6:'Sexta',
 #### pelo computador no intervalo (0 a 50).
 c = 1
 f = 9
+print(np)
 for y in r.values():
     while not (num := input("Descubra numero entre 0 a 50: ")).isdigit() or (n := int(num)) < 0 or int(num) > 50:
         print('Valor INVÁLIDO!')
-        
+       
 
     if int(num) > np and c <= 10:
         print(f"Perdeu a {y} chance, o numero sorteado é menor que {num}")
     elif int(num) < np and c <= 10:
         print(f"Perdeu a {y} chance, o numero sorteado é maior que {num}")
-    elif f != 0 and int(num) != np:
-        print(f"faltam {f} chances!")
-    elif c == 10:
-        print('Suas chances de 10, acabaram')
     elif int(num) == np:
         print(f"Acertou na {y} chance! faltando {f} de 10")
         break
-
+    if f != 0:
+        print(f"faltam {f} chances!")
+    else:
+        print('Suas chances de 10 terminaram.') 
+        
+    
     f -= 1
     c += 1  
