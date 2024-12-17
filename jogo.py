fdc92f1a7f787004b923a6f0379c05f1c8539cf2
shenrique1970 <shvribeiro@hotmail.com>
2024-12-17 05:32:07 -0300

    Nova implementação

--- jogo.py
+++ jogo.py
@@ -1,28 +1,43 @@
-import random
+import random as np
 #import numpy as np
 
 # O jogador deverá escolher um número inteiro qualquer entre 0 e 50.
-np = random.randint(0,50)
+ns = np.randint(0,50)
 
-
-r = {1:'Primeira', 2:'Segunda', 3:'Terceira', 4:'Quarta', 5:'Quinta', 6:'Sexta', 7:'Satima', 8:'Oitava', 9:'Nona', 10:'Decima'}
-
+# nomeando as jogadas
+jogadas = ("Primeira","Segunda","Terceira","Quarta","Quinta","Sexta",
+    "Setima","Oitava","Nona","Decima")
 
 #### Voce terá 10 tentativas para acertar qual foi o número oculto gerado
 #### pelo computador no intervalo (0 a 50).
 c = 1
 f = 9
-print(np)
-for y in r.values():
-    while not (num := input("Descubra numero entre 0 a 50: ")).isdigit() or (n := int(num)) < 0 or int(num) > 50:
-        print('Valor INVÁLIDO!')
+print(ns)
+for y in jogadas:
+    while True:
+        num = input("Descubra um número entre 0 a 50: ")
+        
+        if not num.isdigit():
+            print('Valor INVÁLIDO! Deve ser um número.')
+            continue
+        
+        n = int(num)
+        
+        if n < 0 or n > 50:
+            print('Valor INVÁLIDO! O número deve estar entre 0 e 50.')
+            continue
+        
+        # Se o número for válido, saia do loop
+        break
+
+    print(f'Você digitou um número válido: {n}')
        
 
-    if int(num) > np and c <= 10:
+    if int(num) > ns and c <= 10:
         print(f"Perdeu a {y} chance, o numero sorteado é menor que {num}")
-    elif int(num) < np and c <= 10:
+    elif int(num) < ns and c <= 10:
         print(f"Perdeu a {y} chance, o numero sorteado é maior que {num}")
-    elif int(num) == np:
+    elif int(num) == ns:
         print(f"Acertou na {y} chance! faltando {f} de 10")
         break
     if f != 0:
